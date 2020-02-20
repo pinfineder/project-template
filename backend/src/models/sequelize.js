c61e049b29bfa144d50823c84b4668f6b256bb32
Niklas Halonen <niklas.halonen@eficode.com>
2020-02-20 11:23:23 +0200

    Add postgresql, remove sqlite
    Add wait-for
    Remove backend/db folder
    Remove sqlite

--- backend/src/models/sequelize.js
+++ backend/src/models/sequelize.js
@@ -1,10 +1,20 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize({
+  username: 'root',
+  password: 'password',
+  database: 'project',
+  // This is the service name defined in docker-compose
+  host: 'database',
+  dialect: 'postgres',
   logging: false,
-  dialect: 'sqlite',
-  // This path is relative to /backend folder unlike with require
-  storage: './db/chat.sqlite',
+  operatorsAliases: false,
+  pool: {
+    max: 30,
+    min: 1,
+    idle: 200000,
+    acquire: 1000000,
+  },
 });
 
 module.exports = sequelize;

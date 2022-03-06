3b86fbf038e2464f2c2216f64461cba3d8887240
Niklas Halonen <niklas.halonen@eficode.com>
2022-03-06 16:42:17 +0200

    update frontend

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,49 +1,67 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: './src/index.jsx',
-  devServer: {
-    contentBase: path.join(__dirname, 'src/public'),
-    historyApiFallback: true,
-    disableHostCheck: true,
-    host: process.env.HOST || '0.0.0.0',
-    port: process.env.PORT || 8000,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack')
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+
+const stylesHandler = 'style-loader';
+
+
+
+const config = {
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        open: true, // Open browser once started
+        historyApiFallback: true,
+        host: process.env.HOST || '0.0.0.0', // Listens on all interfaces
+        port: process.env.PORT || 8000,
+
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/public/index.html',
+        }),
+        new webpack.EnvironmentPlugin({
+            'BACKEND_PORT': 9000 // Use 9000 unless BACKEND_PORT is defined in the environment
+        }),
+
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/public/index.html',
-      filename: 'index.html',
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        BACKEND_PORT: process.env.BACKEND_PORT,
-      },
-    })],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: [stylesHandler,'css-loader'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
 };
